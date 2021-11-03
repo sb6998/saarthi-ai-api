8f707fb5be1be78c4957e08ac7c67187621de155
sb6998 <saurabh.bhatia.phe17@iitbhu.ac.in>
2021-11-04 04:51:17 +0530

    debug off

--- saarthi/settings.py
+++ saarthi/settings.py
@@ -20,10 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wjgg_nod+g28^49^cz13p@=)!w@s#s7g(j@5mbj7npk=9--$3='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
